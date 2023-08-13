5545b14b3104cfd575b149356a1a09d3928cc15a
Jaeyeon Jo <jojaeyeon.kr@gmail.com>
2023-08-13 12:34:31 +0900

    day 12 is unreachable.

--- aoc_2022/src/aoc/day12.clj
+++ aoc_2022/src/aoc/day12.clj
@@ -1,5 +1,8 @@
 (ns aoc.day12
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [loom.graph :as graph]
+            [loom.alg :as alg]
+            [loom.io :as io]))
 
 (def file "day12_sample.txt")
 
@@ -8,147 +11,120 @@
   (->> (str/split-lines texts)
        (mapv (comp vec str/trim))))
 
-(comment 
-  (vector '(1 2 3)))
-
-(defn find-start
-  [routes]
-  (let [find-col
-        (fn [x val] (when (= \S val) x))
-        find-row
-        (fn [y row]
-          (when-first [x (keep-indexed find-col row)]
-            [x y]))]
-    (first (keep-indexed find-row routes))))
-
-(defn in-routes?
-  [routes x y]
-  (-> (and (<= 0 y (dec (count routes)))
-           (<= 0 x (dec (count (first routes)))))
-      #_((fn [r] (println "in routes?" [x y r]) r))))  
-
 (defn height
   [s]
-  (int (case s \E \z \S \a s)))
+  (case s
+    \E (inc (int \z))
+    \S (dec (int \a))
+    (int s)))
 
 (defn steppable?
-  [current next]
-  (-> (<= (dec (height next)) (height current) (height next))
-      #_((fn [r] (println "steppable?" [current next r])r))))
-
-(defn remove-visited
-  [distances cx cy neighbours]
- #_ (println "remove from :"
-           [cx cy (get-in distances [cy cx])])
-  (->> neighbours
-       (remove
-        (fn [[x y]]
-          (let [result
-                (<= (get-in distances [y x])
-                    (inc (get-in distances [cy cx])))]
-         #_  (if result
-              (println "\tremoved : " [x y (get-in distances [y x])])
-              (println "\tnot removed : " [x y (get-in distances [y x])]))
-            result)))))
-
-
-(defn neighbours
-  [routes x y]
-  (->> [[1 0] [-1 0] [0 1] [0 -1]]
-       (map (fn [[dx dy]] [(+ x dx) (+ y dy)])) 
-       (filter (fn [[nx ny]] (in-routes? routes nx ny)))
-       (filter (fn [[nx ny]] (steppable?
-                              (get-in routes [y x])
-                              (get-in routes [ny nx]))))))
-
-(defn init-distance
-  [routes [x y :as _start]]
-  (-> (mapv (fn [row] (mapv (constantly Integer/MAX_VALUE) row)) routes)
-      (assoc-in [y x] 0)))
-
-
-(defn print-situation
-  [distances routes cx cy]
-  (print "c : ")
-  (->> (map (fn [[x y]]
-              [(+ x cx)
-               (+ y cy)])
-            [[0 0] [1 0] [-1 0] [0 1] [0 -1]])
-       (map (fn [[x y]]
-              (let [dist (get-in distances [y x])
-                    r    (get-in routes [y x])]
-                (print [x y r dist ]  " "))))
-       doall)
-  (println))
+  [from to] 
+  (and (not= from \E) (not= to \S)
+       (<= (dec (height to)) (height from) (height to))))
 
 (comment 
-  (get-in [] [1 2])
-  
+  (steppable? \S \b)
+  (steppable? \z \E)
+  (->> (range (int \a)
+              (inc (int \z)))
+       ((juxt butlast rest))
+       (apply map (fn [a b] (steppable? a b)))
+       
+       
+       )
+
   )
 
-(defn solve
-  [[queue routes distances]]
-  (if-let [[x y] (last queue)]
-    (let [_ (print-situation distances routes x y)
-          dist (get-in distances [y x])]
-      
-      (if-let [neighbours
-               (->> (neighbours routes x y)
-                    (remove-visited distances x y)
-                    seq)]
-        [(reduce (fn [q n] (cons n q))
-                 (butlast queue)
-                 neighbours)
-         routes
-         (->> neighbours
-              (reduce
-               (fn [curr-dist [nx ny]]
-                 (update-in curr-dist [ny nx] #(min % (inc dist))))
-               distances))
-         (println "n : " neighbours)]
-        [(butlast queue) routes distances
-         (println "no n")]))
-    [queue routes distances]))
+(defn keywordize
+  [x y]
+  (keyword (str x "." y)))
 
-(comment 
-  (cons 3 '(1 2))
-  (last '(3 1 2))
-  (butlast '(3 1 2))
-  (butlast '())
+(defn unkeywordize
+  [pt-key]
+  (->> (clojure.string/split (name pt-key) #"\.")
+       (mapv read-string)))
 
-  (peek [1 2 3])
-  (pop [1 2 3])
-  )
+(defn generate-graph
+  [char-map]
+  (let [height (count char-map)
+        width (count (first char-map))]
+    (->>
+     (for [r (range height)
+           c (range width)]
+       (let [from  (get-in char-map [r c])]
+         [(keywordize r c)
+          (->> [[-1 0] [1 0] [0 1] [0 -1]]
+               (keep
+                (fn [[y x]]
+                  (let [x (+ c x) y (+ r y)
+                        to (get-in char-map [y x])]
+                    (when (and
+                           (<= 0 y (dec height))
+                           (<= 0 x (dec width))
+                           from to
+                           (steppable? from to)) 
+                      [(keywordize y x) 1]))))
+               (into {}))]))
+     (into {})
+     graph/weighted-graph)))
 
-
-(defn final-pos
-  [routes]
-  (let [find-col
-        (fn [y] (fn [x val] (when (= \E val) [x y])))
-
-        find-row
+(defn find-start-end
+  [path-map]
+  (->> path-map
+       (keep-indexed
         (fn [y row]
-          (->>  (keep-indexed (find-col y) row)
-                first))]
-    
-    (-> (keep-indexed find-row routes)
-        first)))
+          (->> (keep-indexed
+                (fn [x ch]
+                  (case ch
+                    \E [:E [y x]]
+                    \S [:S [y x]]
+                    nil)) row)
+               seq
+               )))
+       (apply concat)
+       (into {})))
 
-(defn final-distance 
-  [x y distances]
-  (get-in distances [y x]))
+(comment 
+  (into {} [[1 2] [3 4]])
+  (count '(1 2 3)) 
+  (def g (generate-graph [[\S \b \c] 
+                          [\a \b \c] 
+                          [\c \c \d]]))
+  (alg/dijkstra-path g :0.0 :2.2)
+  
+  
+  (def path-map (read-content (slurp file)))
+  (def start-end (find-start-end path-map))
+  (def start (apply keywordize (:S start-end)))
+  (def end (apply keywordize (:E start-end)))
+  (def graph (generate-graph path-map))
+  (alg/dijkstra-path graph start end)
+  (alg/dijkstra-path graph :20.0 :20.78)
+  (alg/dijkstra-path graph :20.78 :20.88)
+  (alg/dijkstra-path graph :20.89 :20.88)
+  (alg/dijkstra-path graph :19.82 :20.88) 
+  :g 
+  (alg/dijkstra-path graph :19.79 :19.82)
+  (alg/dijkstra-path graph :12.83 :11.83)
+  (alg/dijkstra-path graph :12.83 :19.103)
+  (alg/dijkstra-path graph :19.103 :29.99 )
+  (alg/dijkstra-path graph :27.98 :27.86)
+  (alg/dijkstra-path graph :27.98 :20.88)
+  (alg/dijkstra-path graph :20.0 :12.95)
+  
 
-(defn solve-problem
-  [text]
-  (let [routes (read-content text)
-        queue (list (find-start routes))
-        distances (init-distance routes (find-start routes))]
-    (loop [[queue routes distances]
-           [queue routes distances]] 
-      (if (seq queue) 
-        (recur (solve [queue routes distances])) 
-        (let [[x y] (final-pos routes)]
-          (final-distance x y distances))))))
+
+  (io/view graph)
+
+  ;; End is unreachable.
+  )
+
+
+
+
+
+
 
 (comment
   (solve-problem
